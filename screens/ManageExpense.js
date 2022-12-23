9954186328ee824e906581767e8cdd1f82af3d1b
THI LY TA <92649607+thienly1@users.noreply.github.com>
2022-12-24 00:29:18 +0100

    Adding Update and Cancel Buttons

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import {StyleSheet, View } from "react-native";
 import { useLayoutEffect } from "react";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -14,9 +15,15 @@ function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {}
+  function cancelHandler() {}
+  function confirmHandler(){}
 
   return (
     <View style={styles.container}>
+        <View style={styles.buttons}>
+            <Button style={styles.button} mode='flat' onPress={cancelHandler}>Cancel</Button>
+            <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+        </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -45,6 +52,15 @@ const styles= StyleSheet.create({
         borderTopWidth: 2,
         borderTopColor: GlobalStyles.colors.primary200,
         alignItems:'center'
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
     }
 
 })
