371f289a5ae43a27f3ceccc39d26d0faf5194b00
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-01-11 15:49:11 +0100

    Managing App-wide State and Using Context

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
 import { GlobalStyles } from "./constants/styles";
 import IconButton from "./components/UI/IconButton";
+import ExpensesContextProvider from "./store/expenses-context";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -61,6 +62,7 @@ export default function App() {
   return (
     <>
       <StatusBar style="auto" />
+      <ExpensesContextProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{
           headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
@@ -80,6 +82,7 @@ export default function App() {
           }} />
         </Stack.Navigator>
       </NavigationContainer>
+      </ExpensesContextProvider>
     </>
   );
 }
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,8 +1,19 @@
+import { useContext } from "react";
 import { Text } from "react-native";
 import ExpensesOutput from "../components/Expenses/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
 
 function RecentExpenses() {
-    return <ExpensesOutput expensesPeriod='Last 7 Days' />
+   const expenseCtx = useContext(ExpensesContext);
+
+   const recentExpenses = expenseCtx.expenses.filter((expense)=>{
+    const today = new Date();
+    const date7DaysAgo =  getDateMinusDays(today, 7);
+    return expense.date > date7DaysAgo;
+   });
+
+    return <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' />
 }
 
 export default RecentExpenses;
